9a05e7d81f8c9e24071dcf99e31c8331afa263f2
Nikhil Goel <nikhil@Nikhils-MacBook-Air.local>
2026-03-17 22:31:34 +0530

    Fix: rewrite dispatch_log on_submit - remove empty items row, fix DN/SI creation, add cancel handler

--- snrg_cartons/snrg_cartons/doctype/dispatch_log/dispatch_log.py
+++ snrg_cartons/snrg_cartons/doctype/dispatch_log/dispatch_log.py
@@ -1,10 +1,11 @@
 import frappe
 from frappe.model.document import Document
+from frappe.utils import flt
 
 class DispatchLog(Document):
 	def on_submit(self):
 		self.validate_carton_status()
-		self.update_totals()
+		self.calculate_totals()
 		self.create_delivery_note()
 		self.create_sales_invoice()
 		self.update_carton_box_logs()
@@ -15,67 +16,87 @@ class DispatchLog(Document):
 			status = frappe.db.get_value("Carton Box Log", row.carton_id, "status")
 			if status != "Available":
 				frappe.throw(f"Carton {row.carton_id} is already dispatched or not available.")
-	
-	def update_totals(self):
+
+	def calculate_totals(self):
 		total_cartons = len(self.cartons)
 		total_pieces = 0
 		total_gross_weight = 0
-		
+
 		for row in self.cartons:
 			cbl = frappe.get_doc("Carton Box Log", row.carton_id)
-			total_gross_weight += cbl.gross_weight_kg
-			for item in cbl.items:
-				total_pieces += item.qty
-		
+			total_gross_weight += flt(cbl.gross_weight_kg)
+			for item in (cbl.items or []):
+				total_pieces += flt(item.qty)
+
 		self.db_set('total_cartons', total_cartons)
 		self.db_set('total_pieces', total_pieces)
 		self.db_set('total_gross_weight', total_gross_weight)
-		
+
 	def create_delivery_note(self):
 		dn = frappe.new_doc("Delivery Note")
 		dn.customer = self.customer
 		dn.posting_date = self.dispatch_date
-		
-		# Adding sales order reference
-		dn.append("items", {
-			# Assuming items are mapped correctly, in full implementation you need to make sure to fetch missing required fields of Delivery Note Item as needed.
-		})
-		
+		dn.set_posting_time = 1
+
+		# Collect all items from all cartons
 		for row in self.cartons:
 			cbl = frappe.get_doc("Carton Box Log", row.carton_id)
-			for item in cbl.items:
-				dn.append("items", {
+			for item in (cbl.items or []):
+				dn_item = {
 					"item_code": item.item_code,
+					"item_name": item.item_name,
 					"qty": item.qty,
 					"uom": item.uom,
+					"stock_uom": item.uom,
+					"conversion_factor": 1,
 					"against_sales_order": self.sales_order,
-					"custom_carton_id": row.carton_id
-				})
-				
+				}
+				dn.append("items", dn_item)
+
+		dn.flags.ignore_permissions = True
 		dn.insert()
 		dn.submit()
 		self.db_set("delivery_note", dn.name)
 
 	def create_sales_invoice(self):
-		if not self.delivery_note:
+		dn_name = frappe.db.get_value("Dispatch Log", self.name, "delivery_note")
+		if not dn_name:
 			return
-		
-		from erpnext.stock.doctype.delivery_note.delivery_note import make_sales_invoice
-		si = make_sales_invoice(self.delivery_note)
-		si.insert()
-		si.submit()
-		self.db_set("sales_invoice", si.name)
-		
+
+		try:
+			from erpnext.stock.doctype.delivery_note.delivery_note import make_sales_invoice
+			si = make_sales_invoice(dn_name)
+			si.flags.ignore_permissions = True
+			si.insert()
+			si.submit()
+			self.db_set("sales_invoice", si.name)
+		except Exception as e:
+			frappe.log_error(f"Sales Invoice creation failed for {self.name}: {str(e)}")
+			frappe.msgprint(f"Delivery Note {dn_name} was created but Sales Invoice could not be auto-created. Please create it manually.", indicator="orange", alert=True)
+
 	def update_carton_box_logs(self):
+		dn_name = frappe.db.get_value("Dispatch Log", self.name, "delivery_note")
+		si_name = frappe.db.get_value("Dispatch Log", self.name, "sales_invoice")
+
 		for row in self.cartons:
-			cbl = frappe.get_doc("Carton Box Log", row.carton_id)
-			cbl.status = "Dispatched"
-			cbl.dispatch_log = self.name
-			cbl.delivery_note = self.delivery_note
-			cbl.sales_invoice = self.sales_invoice
-			cbl.customer = self.customer
-			cbl.dispatched_date = self.dispatch_date
-			cbl.save()
-			
+			frappe.db.set_value("Carton Box Log", row.carton_id, {
+				"status": "Dispatched",
+				"dispatch_log": self.name,
+				"delivery_note": dn_name or "",
+				"sales_invoice": si_name or "",
+				"customer": self.customer,
+				"dispatched_date": self.dispatch_date
+			})
+
 	def on_cancel(self):
+		# Revert carton statuses
+		for row in self.cartons:
+			frappe.db.set_value("Carton Box Log", row.carton_id, {
+				"status": "Available",
+				"dispatch_log": "",
+				"delivery_note": "",
+				"sales_invoice": "",
+				"customer": "",
+				"dispatched_date": None
+			})
 		self.db_set('status', 'Cancelled')
